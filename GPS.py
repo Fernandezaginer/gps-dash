a224bef91c63d5b0fc617a692a313730824c48a6
JulianJH22 <166717645+JulianJH22@users.noreply.github.com>
2024-06-22 19:38:57 +0200

    Add files via upload

---
+++ GPS.py
@@ -0,0 +1,30 @@
+import serial
+
+from ublox_gps import UbloxGps
+from smbus2 import SMBus
+
+bus = SMBus(0)
+address = [0x42]
+
+port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
+gps = UbloxGps(port)
+
+def run():
+
+    try:
+        print("Listening for UBX Messages")
+        while True:
+            try:
+                geo = gps.geo_coords()
+                print("Longitude: ", geo.lon) 
+                print("Latitude: ", geo.lat)
+                print("Heading of Motion: ", geo.headMot)
+            except (ValueError, IOError) as err:
+                print(err)
+
+    finally:
+        port.close()
+
+
+if __name__ == '__main__':
+    run()
