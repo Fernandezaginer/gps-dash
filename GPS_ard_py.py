324fd1e391b419547caddb3243bbcef5a3bb44cf
Marcos <130710734+MarcosRGZ38@users.noreply.github.com>
2024-06-25 16:30:54 +0200

    Added Error funcionality
    based on DOP
    Dissolution of position
    built in in nmea coordinates

--- GPS_ard_py.py
+++ GPS_ard_py.py
@@ -1,14 +1,17 @@
 import serial
 import pynmea2
+import math
 
 def parse_nmea_sentence(sentence):
     try:
         parsed_data = pynmea2.parse(sentence)
         if isinstance(parsed_data, pynmea2.GGA):
-            return parsed_data.latitude, parsed_data.longitude, parsed_data.altitude
+            error = sqrt(parsed_data.hdop**2 + parsed_data.vdop**2)
+            return parsed_data.latitude, parsed_data.longitude, parsed_data.altitude, error
     except pynmea2.ParseError:
         pass
     return None
+
 
 def read_gps_data():
     ser = serial.Serial('/dev/ttyACM0', 115200)
@@ -21,14 +24,11 @@ def read_gps_data():
 
 if __name__ == "__main__":
     while True:
-        serialport = serial.Serial('/dev/ttyACM0', 115200)
         position = read_gps_data()
 
         if position:
-            latitude, longitude, altitude = position
-            # print(f"Latitud: {latitude:.9f}, Longitud: {longitude:.9f}, Altitud: {altitude:.2f} metros")
-            
-            # debug purposes
-            print(serialport.readline().decode("utf-8").strip())
+            latitude, longitude, altitude, relative_accuracy = position
+            print(f"Latitud: {latitude:.9f}, Longitud: {longitude:.9f}, Altitud: {altitude:.2f} metros, Precision:{relative_accuracy}")
+
         else:
             print("No se pudo obtener la posición GPS.")
