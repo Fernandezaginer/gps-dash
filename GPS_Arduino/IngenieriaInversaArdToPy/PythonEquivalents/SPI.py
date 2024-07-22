41f5af31a775c0bce38bd9b86b4bcbaf550004ef
Marcos <130710734+MarcosRGZ38@users.noreply.github.com>
2024-07-22 17:41:21 +0200

    n

--- GPS_Arduino/IngenieriaInversaArdToPy/PythonEquivalents/SPI.py
+++ GPS_Arduino/IngenieriaInversaArdToPy/PythonEquivalents/SPI.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import simpy
 import math
+import time
+import psutil
+
 
 if '_SPI_H_INCLUDED' not in locals():
     _SPI_H_INCLUDED:str = "_SPI_H_INCLUDED"
@@ -11,15 +14,17 @@ GPIO.setmode(GPIO.BOARD)
 #import arduino.py #TODO
 
 if '__arm__' in locals() and 'TEENSYDUINO' in locals():
-    pass
-
+    pass 
 
 
 
 if  '__has_include' in locals() and '__has_include_eventResponder.py' in locals():
-    # define SPI_HAS_TRAMSFER_ASYNC = true
-    #define SPI_HAS_TRANSFER_BUF
-    pass
+    SPI_HAS_TRAMSFER_ASYNC:bool = True
+    SPI_HAS_TRANSFER_BUF:bool = True
+
+
+def _BV(var):
+    return (1 << var)
 
 
     
@@ -40,20 +45,23 @@ if 'MSBFIRST' not in locals():
     MSBFIRST:int = 1
     
 
-SPI_MODE0:hex = 0x00
-SPI_MODE1:hex = 0x04
-SPI_MODE2:hex = 0x08
-SPI_MODE3:hex = 0x0C
-SPI_CLOCK_DIV4:hex = 0X00
-SPI_CLOCK_DIV16:hex = 0x01
-SPI_CLOCK_DIV128:hex = 0x03
-SPI_CLOCK_DIV2:hex = 0x04
-SPI_CLOCK_DIV8:hex = 0x05
-SPI_CLOCK_DIV32:hex = 0x06
+SPI_MODE0 = 0x00
+SPI_MODE1 = 0x04
+SPI_MODE2 = 0x08
+SPI_MODE3 = 0x0C
+SPI_CLOCK_DIV4 = 0X00
+SPI_CLOCK_DIV16 = 0x01
+SPI_CLOCK_DIV128 = 0x03
+SPI_CLOCK_DIV2 = 0x04
+SPI_CLOCK_DIV8 = 0x05
+SPI_CLOCK_DIV32 = 0x06
 
-SPI_MODE_MASK:hex = 0x0C
-SPI_CLOCK_MASK:hex = 0x03
-SPI_2XCLOCK_MASK:hex = 0x01
+SPI_MODE_MASK = 0x0C
+SPI_CLOCK_MASK = 0x03
+SPI_2XCLOCK_MASK = 0x01
+
+##PINES 
+SPI_TRANSACTION_MISMATCH_LED:int = 5
 
 
 
@@ -70,6 +78,9 @@ elif 'GICR' in locals():
 elif 'GIMSK' in locals():
     SPI_AVR_EIMSK:str = 'GIMSK'
 
+#Velocidad de la cpu
+
+cpufreq = psutil.cpu_freq()
 
 class SPISettings:
     def SPISettings(clock:int, bitOrder:int, dataMode:int, self):
@@ -84,25 +95,25 @@ class SPISettings:
         init_AlwaysInline(clock, bitOrder, dataMode)
         
 	def init_AlwaysInline(clock:int, bitOrder:int, dataMode:int):
-		clockDiv:int = 0
+		clockDiv = 0
         if clock.is_integer() == True:
             clockDiv = 0
-			if clock >= F_CPU / 2:
+			if clock >= cpufreq / 2:
 				clockDiv = 0
-			elif clock >= F_CPU / 4:
+			elif clock >=  cpufreq / 4:
 				clockDiv = 1
-			elif clock >= F_CPU / 8:
+			elif clock >= cpufreq / 8:
 				clockDiv = 2
-			elif clock >= F_CPU / 16:
+			elif clock >= cpufreq / 16:
 				clockDiv = 3
-			elif clock >= F_CPU / 32:
+			elif clock >= cpufreq / 32:
 				clockDiv = 4
-			elif clock >= F_CPU / 64:
+			elif clock >= cpufreq / 64:
 				clockDiv = 5
 			else:
 				clockDiv = 6
         else:
-			clockSetting:int = F_CPU/2
+		    clockSetting:int = cpufreq/2
             clockDiv = 0
             while clockDiv < 6 and clock < clockSetting:
                   clockSetting = clockSetting/2
@@ -113,10 +124,10 @@ class SPISettings:
         
 		clockDiv ^= 0x01 ## XOR binario
 
-		#DE la librería Avr de arduino, importar TODO 
-		#spcr = _BV(SPE) | _BV(MSTR) | ((bitOrder == LSBFIRST) ? _BV(DORD) : 0) |
+		# De la librería Avr de arduino, importar TODO 
+		# spcr = _BV(SPE) | _BV(MSTR) | ((bitOrder == LSBFIRST) ? _BV(DORD) : 0) |
 		#	(dataMode & SPI_MODE_MASK) | ((clockDiv >> 1) & SPI_CLOCK_MASK);
-		spsr:int = clockDiv && SPI_2XCLOCK_MASK
+		spsr = clockDiv & SPI_2XCLOCK_MASK #Formato binario
 
 
 
@@ -140,11 +151,12 @@ class Out:
    
 
 class SPIClass: #Avr 
+    
     def begin():
         pass
     def usingInterrupt(interruptNumber:int):
-    	pass #registra cosas que usan la libreria SPI, sirve para prevenir conflictos
-	
+        pass #registra cosas que usan la libreria SPI, sirve para prevenir conflictos
+        
     def beginTransaction(settings:SPISettings):
         if interruptMode > 0: #Localizar origen de la variable TODO
             if SPI_AVR_EIMSK in locals():
@@ -159,7 +171,7 @@ class SPIClass: #Avr
         if 'SPI_TRANSACTION_MISMATCH_LED' in locals():
             if(inTransactionFlag):
                 GPIO.setup(SPI_TRANSACTION_MISMATCH_LED, GPIO.OUT)
-                GPIO.setup(SPI_TRANSACTION_MISMATCH_LED, GPIO.HIGH)
+                GPIO.output(SPI_TRANSACTION_MISMATCH_LED, GPIO.HIGH)
 
                 inTransactionFlag:int = 1
 
@@ -169,15 +181,15 @@ class SPIClass: #Avr
     def transfer(data:int) -> int:
         SPDR:int = data
         volatile:str = "volatile"
-        while not SPDR and _BV(SPIF): #TODO
+        while  (~SPDR)&_BV(SPIF): #TODO
             return SPDR
 
     def transfer16(data:int) -> int:
         InContainer:In
         OutContainer:Out
         In.val = data
-        if 'SPDR' not in locals() and _BV(DORD): #TODO
-            SPDR = InContainer.lsMsb.Lsb
+        if 'SPDR' not in locals() and _BV(DORD) in locals(): #TODO
+            SPDR = InContainer.lsbMsb.Lsb
 
             while not 'SPSR' in locals() and _BV(SPIF): #TODO
                 OutContainer.lsMsb.Lsb = SPDR
@@ -202,19 +214,71 @@ class SPIClass: #Avr
         i = 1
         while count > 1:
             out = p[i]
-            while not (SPSR & (1 << SPIF)):
+            while  ~(SPSR & (1 << SPIF)):
                 pass
-            in_ = SPDR
+            in_:int = SPDR
             SPDR = out
             p[i - 1] = in_
             count -= 1
             i += 1
 
-        while not (SPSR and (1 << SPIF)):
+        while  ~(SPSR&_BV(SPIF)): #TODO 
             pass
         p[i - 1] = SPDR
 
-        return bytes(p)
+    def setTransferWriteFill(ch:int):
+         _transferWriteFill:int = ch
+    
+    def transfer(buf,retbuf,count:int):
+        pass
+
+    def endTransaction():
+        if 'SPI_TRANSACTION_MISMATCH_LED' in locals():
+            if inTransactionflag == 0: #TODO define variable
+                GPIO.setup(SPI_TRANSACTION_MISMATCH_LED, GPIO.OUT)
+                GPIO.output(SPI_TRANSACTION_MISMATCH_LED, GPIO.HIGH)
+
+            inTransactionflag = 0
+    
+        if interruptMode > 0: #TODO
+            if 'SPI_AVR_EIMSK' in locals():
+                if interruptMode == 1:
+                    SPI_AVR_EIMSK = interruptSave #TODO 
+                else:
+                    SREG = interruptSave
+    
+    end() ## finalizar el buf SPI #TODO
+
+    def setBitOrder(bitOrder:int):
+        if bitOrder == LSBFIRST:
+            SPCR = SPCR | _BV(SPCR)
+        
+        else:
+            SPCR = SPCR & ~_BV(DORD)
+
+    def setDataMode(datamode:int):
+        SPCR = (SPCR & ~SPI_MODE_MASK)| datamode
+
+    def setClockDivider(clockDiv:int):
+        SPCR = (SPCR & ~SPI_CLOCK_MASK)|clockDiv
+        SPSR = (SPSR & ~SPI_2XCLOCK_MASK)|(clockDiv & SPI_CLOCK_MASK)
+
+    def attachInterrupt():
+        SPCR = SPIE | _BV(SPIE)
+    def detatchInterrupt():
+        SPCR = SPCR & ~_BV(SPIE)
+    
+    def Teensyduino_Test_constinit(instance:int,index:int) -> int:
+        pass
+    
+
+    
+
+
+
+
+                
+                
 
 
 
