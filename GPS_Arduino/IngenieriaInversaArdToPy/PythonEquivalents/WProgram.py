4b3d6862c206f208f6f38ab3c14fdea2c2623330
Marcos <130710734+MarcosRGZ38@users.noreply.github.com>
2024-07-30 00:52:40 +0200

    ongoing

---
+++ GPS_Arduino/IngenieriaInversaArdToPy/PythonEquivalents/WProgram.py
@@ -0,0 +1,72 @@
+import math
+import string
+import random
+import RPi.GPIO as GPIO
+
+import wiring.py #TODO 
+import WCharacter.py #TODO 
+import WString.py #TODO 
+import serial #HardWareSerial
+
+def makeWord(w) -> int:
+    pass
+
+def makeWordBytewise(h,l) -> int:
+    pass
+
+word = makeWord(__VA_ARGS__)
+
+def pulseIn(pin, state, timeout = 1000000L) -> int:
+    pass
+
+def tone(_pin, frequency, duration = 0):
+    pass
+
+def noTone(_pin):
+    pass
+
+def random1(num) -> int:
+    pass
+
+def random2(num1, num2) -> int:
+    tuple = [num1,num2]
+    return random.choice(tuple)
+
+def randomSeed(int:int):
+    pass
+
+def map(l1:int,l2:int,l3:int,l4:int,l5:int):
+    pass
+
+#Pines GPIO
+GPIO.setup(GPIO.BOARD)
+A0 = 0
+A1 = 1
+A2 = 2
+A3 = 3
+A4 = 4
+A5 = 5
+A6 = 6
+A7 = 7
+A8 = 8
+A9 = 9
+A10 = 10
+A12 = 12
+A13 = 13
+A14 = 14
+A15 = 15
+A16 = 16
+A18 = 18
+A19 = 19
+A20 = 20
+A21 = 21
+A22 = 22
+A24 = 23
+A25 = 25
+A26 = 26
+
+
+
+
+
+
--- GPS_Arduino/IngenieriaInversaArdToPy/PythonEquivalents/Wire.py
+++ GPS_Arduino/IngenieriaInversaArdToPy/PythonEquivalents/Wire.py
@@ -1,67 +1,72 @@
 
 
-    #include <inttypes.h>
-    #include "Stream.h"
-
-        #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_TWOWIRE)
-            extern TwoWire Wire;
-        #endif
+import inttypes.py    
+import Stream.py   
 
 
-        #ifndef I2C_BUFFER_LENGTH
-            #define I2C_BUFFER_LENGTH 128
-        #endif
 
-    class TwoWire: public Stream {
-        private:
-            static uint8_t rxBuffer[];
-            static size_t  rxBufferIndex;
-            static size_t  rxBufferLength;
+if 'I2C_BUFFER_LENGTH' not in locals():
+    I2C_BUFFER_LENGTH = 128
 
-            static uint8_t txAddress;
-            static uint8_t txBuffer[];
-            static size_t  txBufferIndex;
-            static size_t  txBufferLength;
 
-            static uint8_t transmitting;
-            static void (*user_onRequest)(void);
-            static void (*user_onReceive)(size_t);
-            static void onRequestService(void);
-            static void onReceiveService(uint8_t*, size_t);
+class TwoWire:
+        
+        def __init__(self, rxBuffer:list, txAdress, txBuffer:list, trasnmitting): 
+            self.rxBuffer =  rxBuffer
+            self.txAddress = txAdress
+            self.txBuffer = txBuffer
+            self.transmitting = trasnmitting
+    
+        def user_onRequest():
+              pass
+        
+        def user_onReceive(size):
+              pass
+        
+        def onRequestService():
+            pass
+        
+        def onReceiveService(buffer, bufferSize):
+              pass
 
-        public:
-            TwoWire();
-            void   begin(int sda, int scl);
-            void   begin(int sda, int scl, uint8_t address);
-            void   pins(int sda, int scl) __attribute__((deprecated));  // use begin(sda, scl) in new code
-            void   begin();
-            void   begin(uint8_t);
-            void   begin(int);
-            void   setClock(uint32_t);
-            void   setClockStretchLimit(uint32_t);
-            void   beginTransmission(uint8_t);
-            void   beginTransmission(int);
-            uint8_t endTransmission(void);
-            uint8_t endTransmission(uint8_t);
-            size_t  requestFrom(uint8_t address, size_t size, bool sendStop);
-            uint8_t status();
+        def begin(sda, scl, address): 
+            pass
+        def pins(sda, scl):
+            TwoWire.begin(sda, scl)
+        
+        def setClock(time):
+             pass
+        
+        def setClockStretchLimit(stretch):
+             pass
+        
+        def beginTransmission(uint8_t):
+             pass
+        
+        def endTransmission(uint8_t) -> int:
+             pass
+        def  requestFrom(uint8_t address, size_t size, bool sendStop) -> int:
+             pass
+        
+        def status() -> int:
+             pass
 
-            uint8_t requestFrom(uint8_t, uint8_t);
-            uint8_t requestFrom(uint8_t, uint8_t, uint8_t);
-            uint8_t requestFrom(int, int);
-            uint8_t requestFrom(int, int, int);
 
-            virtual size_t write(uint8_t);
-            virtual size_t write(const uint8_t*, size_t);
-            virtual int    available(void);
-            virtual int    read(void);
-            virtual int    peek(void);
-            virtual void   flush(void);
-            void           onReceive(void (*)(int));     // arduino api
-            void           onReceive(void (*)(size_t));  // legacy esp8266 backward compatibility
-            void           onRequest(void (*)(void));
-
-            using Print::write;
-    };
+        def write(buff):
+             pass
+        def available():
+             pass
+        def read(buff:int):
+             pass
+        def peek() -> int:
+             pass
+             
+        def flush():
+             pass
+        
+        def onReceive(buf:int):
+            pass
 
        
+if 'NO_GLOBAL_INSTANCES' not in locals() and 'NO_GLOBAL_TWOWIRE' not in locals():
+    wire:TwoWire = None
